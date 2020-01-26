ceaba4e47b0ddd275a8d86da74e8c8aa3c47a83e
rypkema <rypkema@mit.edu>
2020-01-25 19:51:27 -0500

    added cpp structure

---
+++ src/cpp/app_depth.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <sstream>
+#include <zmq.hpp>
+#include "flatbuffers/flatbuffers.h"
+#include "minnow_comms/minnow_app_threaded.h"
+// flatbuffer topics
+#include "topics/nav_depth_generated.h"
+// depth driver MS5837
+#include "minnow_drivers/cppMS5837/cppMS5837.h"
+
+class AppDepth : public App
+{
+public:
+  AppDepth();
+  ~AppDepth();
+
+protected:
+  void Process();
+  void Init();
+  void CallBack(uint8_t* msg, size_t msg_size, std::string topic);
+
+private:
+  void SetMessageNavDepth();
+
+  flatbuffers::FlatBufferBuilder* builder;
+  uint8_t* msg_buf;
+  unsigned int msg_size;
+  MS5837 depth_sensor;
+};
+
+AppDepth::AppDepth() {
+}
+
+AppDepth::~AppDepth() {
+}
+
+void AppDepth::CallBack(uint8_t* msg, size_t msg_size, std::string topic) {
+  std::cout << "Callback received for topic \"" << topic << "\"" << std::endl;
+  std::string msg_str(msg, msg+msg_size); // required for beaglebone
+}
+
+void AppDepth::SetMessageNavDepth() {
+  builder->Clear();
+  auto now     = std::chrono::system_clock::now();
+  auto epoch   = now.time_since_epoch();
+  auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
+  double utc = milliseconds.count()/1000.0;
+  double val;
+
+  topics::nav::depthBuilder depth_builder(*builder);
+  depth_builder.add_time(utc);
+  val = depth_sensor.temperature();
+  depth_builder.add_temperature(val);
+  val = depth_sensor.pressure();
+  depth_builder.add_pressure(val);
+  val = depth_sensor.depth();
+  depth_builder.add_depth_fluid(val);
+
+  auto depth_msg = depth_builder.Finish();
+  builder->Finish(depth_msg);
+  msg_buf = builder->GetBufferPointer();
+  msg_size = builder->GetSize();
+}
+
+void AppDepth::Init() {
+  unsigned int tick = GetConfigParameter<unsigned int>("tick");
+  SetHz(tick);
+
+  float fluid_density = GetConfigParameter<float>("fluid_density");
+  unsigned int oversampling = GetConfigParameter<unsigned int>("oversampling");
+  depth_sensor.init();
+	depth_sensor.setModel(MS5837::MS5837_30BA);
+  depth_sensor.setFluidDensity(fluid_density);
+  depth_sensor.setOverSampling(oversampling);
+
+  builder = new flatbuffers::FlatBufferBuilder();
+}
+
+void AppDepth::Process() {
+  SetMessageNavDepth();
+  depth_sensor.read();
+  Publish("nav.depth", msg_buf, msg_size);
+}
+
+int main(int argc, char *argv[])
+{
+  if(argc < 2) {
+    std::cout << "Expected a YAML configuration file... Exiting." << std::endl;
+    exit(0);
+  }
+  std::string config_file = argv[1];
+  AppDepth app_depth;
+  app_depth.SetName("app_depth");
+  app_depth.SetConfig(config_file);
+  app_depth.Run();
+}
---
+++ src/cpp/minnow_comms/minnow_app_threaded.cpp
@@ -0,0 +1,235 @@
+#include <iostream>
+#include <sstream>
+#include <signal.h>
+#include <zmq.hpp>
+#include "minnow_app_threaded.h"
+
+using namespace std::chrono;
+
+Subscriber::Subscriber(App* app, std::string address, std::string topic, std::function<void(uint8_t* msg, size_t msg_size, std::string topic)> callback) : receive_active_(false), new_msg_(false) {
+  app_ = app;
+  zmq_context_ = app_->zmq_context_;
+  address_ = address;
+  topic_ = topic;
+  callback_ = callback;
+}
+
+Subscriber::~Subscriber() {
+  receive_thread_.join();
+}
+
+void Subscriber::Start() {
+  receive_active_ = true;
+  receive_thread_ = std::thread(&Subscriber::Run, this);
+}
+
+void Subscriber::Stop() {
+  receive_active_ = false;
+}
+
+void Subscriber::Run() {
+  std::stringstream ss;
+  ss << "[T] Starting subscriber thread for topic: " << topic_;
+  Print(ss.str());
+  ss.str("");
+
+  // variables for message matching
+  std::string msg_str;
+  size_t msg_size;
+  bool subscribed;
+
+  zmq::socket_t subscriber(*zmq_context_, ZMQ_SUB);
+  zmq_connect((void*)subscriber, address_.c_str());
+  zmq_setsockopt((void*)subscriber, ZMQ_SUBSCRIBE, topic_.c_str(), topic_.size());
+  // std::vector<zmq::pollitem_t> p = {{subscriber, 0, ZMQ_POLLIN, 0}};
+  zmq::pollitem_t p[] = {{(void*)subscriber, 0, ZMQ_POLLIN, 0}};
+  while(receive_active_) {
+    // zmq_poll(p.data(), 1, 1000);
+    zmq_poll(&p[0], 1, 1000);
+    if (p[0].revents & ZMQ_POLLIN) {
+      mutex_.lock();
+      subscriber.recv(&msg_, ZMQ_DONTWAIT);
+      // copy message into local variable for checking
+      msg_str.assign(static_cast<char *>(msg_.data()), msg_.size());
+      msg_size = msg_.size();
+      mutex_.unlock();
+
+      // check for message match - if not a match, spin up a new subscriber for the new message
+      size_t msg_start = msg_str.find_first_of("_");
+      std::string topic = msg_str.substr(0,msg_start);
+      if(topic == topic_) {
+        new_msg_ = true;
+      } else {
+        new_msg_ = false;
+        subscribed = app_->CheckSubscriptionToTopic(topic);
+        if(!subscribed) {
+          app_->Subscribe(topic, callback_);
+        }
+      }
+    }
+  }
+
+  ss << "[T] Stopping subscriber thread for topic: " << topic_;
+  Print(ss.str());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+App::App() {
+  zmq_context_ = new zmq::context_t(1);
+  zmq_socket_ = new zmq::socket_t(*zmq_context_, ZMQ_PUB);
+  sleep_us_ = 10000;  // 100Hz default
+
+  cpphandler = std::bind(&App::ExitSignal, this, std::placeholders::_1);
+  struct sigaction sigIntHandler;
+  sigIntHandler.sa_handler = signal_handler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+  sigaction(SIGINT, &sigIntHandler, NULL);
+}
+
+App::~App() {
+  for(std::vector<Subscriber*>::iterator it = subscriptions_.begin(); it != subscriptions_.end(); ++it) {
+    delete *it;
+  }
+  delete zmq_socket_;
+  delete zmq_context_;
+}
+
+void App::ExitSignal(int s) {
+  std::stringstream ss;
+  for(std::vector<Subscriber*>::iterator it = subscriptions_.begin(); it != subscriptions_.end(); ++it) {
+    ss << "[M] Subscription for " << (*it)->topic_ << " killed.";
+    Print(ss.str());
+    ss.str("");
+
+    (*it)->Stop();
+  }
+  std::this_thread::sleep_for(milliseconds(1500));
+  Print("Exiting Minnow App...");
+  zmq_ctx_destroy(zmq_context_);
+  exit(1);
+}
+
+void App::Subscribe(std::string topic, std::function<void(uint8_t* msg, size_t msg_size, std::string topic)> callback) {
+  std::stringstream ss;
+  ss << "[M] Subscription for " << topic << " created.";
+  Print(ss.str());
+
+  Subscriber* sub = new Subscriber(this, subscriber_address_, topic, callback);
+  mutex_.lock();
+  subscriptions_.push_back(sub);
+  subscription_topics_.push_back(topic);
+  mutex_.unlock();
+  sub->Start();
+}
+
+bool App::CheckSubscriptionToTopic(std::string topic) {
+  bool subscribed = false;
+  for(std::vector<std::string>::iterator it = subscription_topics_.begin(); it != subscription_topics_.end(); ++it) {
+    if(topic == (*it)) subscribed = true;
+  }
+  return subscribed;
+}
+
+void App::CheckSubscriptions() {
+  std::stringstream ss;
+  mutex_.lock();
+  for(std::vector<Subscriber*>::iterator it = subscriptions_.begin(); it != subscriptions_.end(); ++it) {
+    if((*it)->new_msg_) {
+      ss << "[M] New message from " << (*it)->topic_ << ".";
+      Print(ss.str());
+      ss.str("");
+      (*it)->new_msg_ = false;
+      std::string msg_str;
+      size_t msg_size;
+      (*it)->mutex_.lock();
+      msg_str.assign(static_cast<char *>((*it)->msg_.data()), (*it)->msg_.size());
+      msg_size = (*it)->msg_.size();
+      (*it)->mutex_.unlock();
+      size_t msg_start = msg_str.find_first_of("_");
+      (*it)->callback_((uint8_t*)msg_str.data()+(msg_start+1), msg_size-(msg_start+1), msg_str.substr(0,msg_start));
+    }
+  }
+  mutex_.unlock();
+}
+
+void App::PublishString(std::string topic, std::string msg, size_t msg_size) {
+  Publish(topic, (uint8_t*)msg.data(), msg.length());
+}
+
+void App::Publish(std::string topic, uint8_t* msg, size_t msg_size) {
+  topic.append("_");
+  unsigned int total_len = msg_size + topic.length();
+  uint8_t *buffer= new uint8_t[total_len];
+  memcpy(buffer, topic.data(), topic.length());
+  memcpy(buffer+topic.length(), msg, msg_size);
+  int rc = zmq_send((void*)*zmq_socket_, buffer, (size_t)total_len, 0);
+}
+
+void App::SetConfig(std::string config_file) {
+  try {
+		config_ = YAML::LoadFile(config_file);
+	} catch (...) {
+		std::cout << "[C] Configuration file \"" << config_file << "\" not found... Exiting." << std::endl;
+		exit(0);
+	}
+  try {
+    protocol_ = config_["protocol"].as<std::string>();
+    host_ = config_["host"].as<std::string>();
+    if(protocol_ == "ipc") {
+      std::cout << "[C] Using inter-process communications (ipc)." << std::endl;
+    } else if(protocol_ == "tcp") {
+      std::cout << "[C] Using transmission control protocol (tcp)." << std::endl;
+      port_ = config_["port"].as<int>();
+    } else {
+      std::cout << "[C] Unknown communications protocol... Exiting." << std::endl;
+      exit(0);
+    }
+  } catch (...) {
+    std::cout << "[C] Configuration file must define \"protocol\", \"host\" and, if required, \"port\"... Exiting." << std::endl;
+    exit(0);
+  }
+}
+
+void App::Run() {
+  std::stringstream ss;
+  ss << "[M] Starting Minnow App \"" << name_ << "\"!";
+  Print(ss.str());
+  ss.str("");
+
+  if(protocol_ == "ipc") {
+    ss << "ipc://" << host_ << ".sub";
+  } else if(protocol_ == "tcp") {
+    ss << "tcp://" << host_ << ":" << port_+1;
+  }
+  std::string socket = ss.str();
+  std::cout << "[C] Publishing address: " << socket << std::endl;
+  zmq_connect((void*)*zmq_socket_, socket.c_str());
+  ss.str("");
+  if(protocol_ == "ipc") {
+    ss << "ipc://" << host_ << ".pub";
+  } else if(protocol_ == "tcp") {
+    ss << "tcp://" << host_ << ":" << port_;
+  }
+  subscriber_address_ = ss.str();
+  std::cout << "[C] Subscribing address: " << subscriber_address_ << std::endl;
+
+  Init();
+
+  while(true) {
+    if(sleep_us_ != 0) loop_start_ = high_resolution_clock::now();
+
+    CheckSubscriptions();
+    Process();
+
+    if(sleep_us_ != 0) {
+      loop_end_ = high_resolution_clock::now();
+      loop_us_ = duration_cast<microseconds>(loop_end_ - loop_start_);
+      int rem_usecs = sleep_us_ - loop_us_.count();
+      if(rem_usecs > 0) {
+        std::this_thread::sleep_for(microseconds(rem_usecs));
+      }
+    }
+  }
+}
